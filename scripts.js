f0a769da61ca3e3b5227c5aa5ce5c89ba5f78aee
Janshai <jack.r.s.adams@gmail.com>
2018-04-25 10:02:53 +0100

    Change Button Layout

--- scripts.js
+++ scripts.js
@@ -2,20 +2,25 @@ function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     if (/windows phone/i.test(userAgent)) {
-        document.getElementById("appDownloadButton").innerText = "Only available on Android and iOS"
-        document.getElementById("appDownloadButton").disabled = true
+        document.getElementById("androidappDownloadButton").innerText = "Only available on Android and iOS"
+        document.getElementById("androidappDownloadButton").disabled = true
+        document.getElementById("iosappDownloadButton").innerText = "Only available on Android and iOS"
+        document.getElementById("iosappDownloadButton").disabled = true
 
     }
     else if (/android/i.test(userAgent)) {
-        document.getElementById("link").href = "http://www.cnn.com/";
+        document.getElementById("iosappDownloadButton").innerText = "Android App"
+        document.getElementById("iosappDownloadButton").disabled = true
     }
     else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         document.getElementById("androidappDownloadButton").innerText = "Android App"
         document.getElementById("androidappDownloadButton").disabled = true
     }
     else {
-        document.getElementById("appDownloadButton").innerText = "Only available on Android and iOS"
-        document.getElementById("appDownloadButton").disabled = true
+        document.getElementById("androidappDownloadButton").innerText = "Only available on Android and iOS"
+        document.getElementById("androidappDownloadButton").disabled = true
+        document.getElementById("iosappDownloadButton").innerText = "Only available on Android and iOS"
+        document.getElementById("iosappDownloadButton").disabled = true
 
     }
 
